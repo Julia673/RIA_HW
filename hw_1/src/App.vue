6f229d414b64a56cd2880e3a260b98a09358a8b8
Julia673 <61210450+Julia673@users.noreply.github.com>
2021-03-05 13:32:36 +0200

    Create App.vue

---
+++ hw_1/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" class="d-flex justify-content-center">
+    <div class="w-75 p-3">
+      <h2>Інформація про користувача</h2>
+      <hr>
+      <AddUser
+          :users = "users"
+      />
+      <hr>
+      <p class="text-lg-left">Кількість користувачів: <b>0</b></p>
+      <UserList
+          :users = "users"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AddUser from "@/components/AddUser";
+import UserList from "@/components/UserList";
+export default {
+  name: 'App',
+  components: {
+    AddUser,
+    UserList
+  },
+  data() {
+    return {
+      users: []
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
